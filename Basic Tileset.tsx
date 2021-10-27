f9460a98ed41848446071d176cf07afdb0d21cc2
Sophia <reinholdtsoph@gmail.com>
2021-10-27 21:35:59 +0200

    Revert "Deletion Tiles"
    This reverts commit d1af5c30416d2ee0d484c5af794efef42754a683.

---
+++ Basic Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Basic Tileset" tilewidth="32" tileheight="32" tilecount="336" columns="14">
+ <image source="../../../../../Users/z004bsnr/Downloads/TopDownTileset.png" width="448" height="768"/>
+</tileset>
---
+++ Interior Basics.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="free_overview" tilewidth="32" tileheight="32" tilecount="3248" columns="58">
+ <image source="../../../../../Users/z004bsnr/Desktop/free_overview.png" width="1886" height="1792"/>
+</tileset>
