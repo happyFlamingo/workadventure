297dc0e4b8d39bb893a917db44d80b013ec129cb
Sophia <reinholdtsoph@gmail.com>
2021-10-27 21:36:06 +0200

    Revert "New Tilesets"
    This reverts commit f7136542a2bd16c6d19a4ca5940d29cec0dd6ed1.

--- Modern_Office_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Modern_Office_32x32" tilewidth="32" tileheight="32" tilecount="848" columns="16">
- <image source="../../../../../Users/z004bsnr/Desktop/Modern_Office_32x32.png" width="512" height="1696"/>
-</tileset>
